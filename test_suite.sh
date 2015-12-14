50376dfd8f5fea281a0d20989263096ee6ed7db2
Lally Singh <yell@lal.ly>
2015-12-14 16:30:23 -0500

    Added a max-client arg.

--- test_suite.sh
+++ test_suite.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #trap "" SIGCHLD
-DEFAULT_WAIT=10
-WAIT=${1:-$DEFAULT_WAIT}
+WAIT=${1:-10}
+NCLIENTS=${2:-1000}
 
-pkill node; sleep 1; 
-for i in $(seq 100 100 1000)
-do 
-  (cd ../freedom-social-quiver-server; npm start >/dev/null) &
-  sleep 1
-  node single-client.js -p $i -m 10 -w $WAIT http://localhost:8080
-  pkill -INT node
-  sleep 5
+pkill node; sleep 1;
+for i in $(seq 100 $(((NCLIENTS-100) / 10)) $NCLIENTS)
+do
+    (cd ../freedom-social-quiver-server; npm start >/dev/null) &
+    sleep 1
+    node single-client.js -p $i -m 10 -w $WAIT http://localhost:8080
+    pkill -INT node
+    sleep 5
 done
 
